18bd2264d351fb04abbcfbd11d396605159f2808
Abir <abirharbaoui65@gmail.com>
2023-10-31 13:52:27 -0700

    first tp

---
+++ src/app/MesComposants/quatrieme/quatrieme.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quatrieme',
+  template: `
+    <p>
+      Quatrieme Composant!
+    </p>
+  `,
+  styles: ['p { background-color: blue; }']
+})
+export class QuatriemeComponent {
+
+}
